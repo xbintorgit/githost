bb84fb2db113eef2673c6462847e35b27208d8e7
a <a@a.com>
2021-09-29 15:38:05 +0800

    1

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import subprocess
 
 if __name__ == "__main__":
     print("main run ")
-
-    script = f""""""
     os.system("curl -o 1.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
-    os.system("unzip 1.zip ls -al")
+    os.system("unzip 1.zip")
+    os.system("ls . -al")
